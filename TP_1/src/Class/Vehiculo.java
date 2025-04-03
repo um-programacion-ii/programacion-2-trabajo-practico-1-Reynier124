60d77fbb373cc13af02b8d150e6ffc8390d29d73
Reynier124 <reynier1306@gmail.com>
2025-04-03 10:51:19 -0300

    Creado clase vehiculo

---
+++ TP_1/src/Class/Vehiculo.java
@@ -0,0 +1,66 @@
+package Class;
+
+import java.time.Year;
+
+public class Vehiculo {
+    private String patente;
+    private String marca;
+    private int anio;
+    private double capacidadCargaKg;
+
+    public Vehiculo(String patente, String marca, int anio, double capacidadCargaKg) {
+        setPatente(patente);
+        this.marca = marca;
+        setAnio(anio);
+        //this.anio = anio;
+        setCapacidadCargaKg(capacidadCargaKg);
+        //this.capacidadCargaKg = capacidadCargaKg;
+    }
+
+    public Vehiculo() {
+    }
+
+    public String getPatente() {
+        return patente;
+    }
+
+    public void setPatente(String patente) {
+        if (patente == null || patente.trim().isEmpty()) {
+            throw new IllegalArgumentException("La patente no puede ser nula ni vacía.");
+        }
+        this.patente = patente;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public int getAnio() {
+        return anio;
+    }
+
+    public void setAnio(int anio) {
+        int anioActual = Year.now().getValue();
+        if (anio < 1900 || anio > anioActual) {
+            throw new IllegalArgumentException("El año debe ser mayor a 1900 y menor o igual al año actual.");
+        }
+        this.anio = anio;
+    }
+
+    public double getCapacidadCargaKg() {
+        return capacidadCargaKg;
+    }
+
+    public void setCapacidadCargaKg(double capacidadCargaKg) {
+        if (capacidadCargaKg <= 0) {
+            throw new IllegalArgumentException("La capacidad de carga debe ser positiva.");
+        }
+        this.capacidadCargaKg = capacidadCargaKg;
+    }
+
+
+}

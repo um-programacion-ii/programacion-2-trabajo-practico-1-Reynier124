1e703e4d76041c5410d1e23744e95dd74b082c53
Reynier124 <reynier1306@gmail.com>
2025-04-03 11:11:53 -0300

    Añadiendo funcionalidad a la clase vehiculo

--- TP_1/src/Class/Vehiculo.java
+++ TP_1/src/Class/Vehiculo.java
@@ -8,4 +8,66 @@ public class Vehiculo {
     private int anio;
     private double capacidadCargaKg;
 
+    public Vehiculo(String patente, String marca, int anio, double capacidadCargaKg) {
+        setPatente(patente);
+        this.marca = marca;
+        setAnio(anio);
+        //this.anio = anio;
+        setCapacidadCargaKg(capacidadCargaKg);
+        //this.capacidadCargaKg = capacidadCargaKg;
+    }
+
+    public Vehiculo() {
+    }
+
+    public String getPatente() {
+        return patente;
+    }
+
+    public void setPatente(String patente) {
+        if (patente == null || patente.trim().isEmpty()) {
+            throw new IllegalArgumentException("La patente no puede ser nula ni vacía.");
+        }
+        this.patente = patente;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public int getAnio() {
+        return anio;
+    }
+
+    public void setAnio(int anio) {
+        int anioActual = Year.now().getValue();
+        if (anio < 1900 || anio > anioActual) {
+            throw new IllegalArgumentException("El año debe ser mayor a 1900 y menor o igual al año actual.");
+        }
+        this.anio = anio;
+    }
+
+    public double getCapacidadCargaKg() {
+        return capacidadCargaKg;
+    }
+
+    public void setCapacidadCargaKg(double capacidadCargaKg) {
+        if (capacidadCargaKg <= 0) {
+            throw new IllegalArgumentException("La capacidad de carga debe ser positiva.");
+        }
+        this.capacidadCargaKg = capacidadCargaKg;
+    }
+
+    public void  mostrarInformacion(){
+        System.out.println("El vehiculo tiene los siguientes datos:");
+        System.out.println("La patente: "+this.getPatente());
+        System.out.println("La marca: "+this.getMarca());
+        System.out.println("El año: "+this.getAnio());
+        System.out.println("La capacitad de carga en kg: "+this.getCapacidadCargaKg());
+    }
+
 }

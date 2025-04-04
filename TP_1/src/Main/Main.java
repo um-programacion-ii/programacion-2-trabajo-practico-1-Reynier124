4b62a3710f6350548ebcf9e565b97762f066bc3b
Reynier124 <reynier1306@gmail.com>
2025-04-04 12:13:19 -0300

    Arraylist utilizada en main

--- TP_1/src/Main/Main.java
+++ TP_1/src/Main/Main.java
@@ -1,20 +1,28 @@
 package Main;
-
+import Class.Vehiculo;
 import  Class.VehiculoPrinter;
 import Class.Camion;
 import Class.Auto;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class Main {
     public static void main(String[] args) {
         VehiculoPrinter vp = new VehiculoPrinter();
-        
+
+        List<Vehiculo> vehiculos = new ArrayList<>();
+
         Camion c1 = new Camion("123RTYMA", "Renault", 2003, 903.23, true);
         Auto a1 = new Auto("093MAD123", "Ferrari", 2018, 83, 4);
 
-        vp.mostrarDatos(a1);
+        vehiculos.add(c1);
+        vehiculos.add(a1);
+
+        vp.mostrarDatos(vehiculos.get(0));
         System.out.println("---------");
-        vp.mostrarDatos(c1);
+        vp.mostrarDatos(vehiculos.get(1));
     }
 
 }

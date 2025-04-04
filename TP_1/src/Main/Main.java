d4b1f3017a2092b9279af5d6e8288c1e299f9f40
Reynier124 <reynier1306@gmail.com>
2025-04-04 11:28:48 -0300

    Probado la funcionalidad de auto

--- TP_1/src/Main/Main.java
+++ TP_1/src/Main/Main.java
@@ -2,6 +2,7 @@ package Main;
 import Class.Vehiculo;
 import  Class.VehiculoPrinter;
 import Class.Camion;
+import Class.Auto;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
@@ -19,10 +20,13 @@ public class Main {
         System.out.println("---------");
         Vehiculo v1 = new Vehiculo("465LAK21", "Audi", 2007, 56);
         Camion c1 = new Camion("123RTYMA", "Renault", 2003, 903.23, true);
+        Auto a1 = new Auto("093MAD123", "Ferrari", 2018, 83, 4);
 
         vp.mostrarDatos(v1);
         System.out.println("---------");
         vp.mostrarDatos(c1);
+        System.out.println("---------");
+        vp.mostrarDatos(a1);
     }
 
 }

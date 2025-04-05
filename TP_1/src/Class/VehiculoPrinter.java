a733604adf63aee9f5197a35082a5eec4524bda6
Reynier124 <reynier1306@gmail.com>
2025-04-05 10:37:04 -0300

    Correcciones

--- TP_1/src/Class/VehiculoPrinter.java
+++ TP_1/src/Class/VehiculoPrinter.java
@@ -5,11 +5,6 @@ public class VehiculoPrinter {
     }
 
     public void mostrarDatos(Vehiculo v) {
-        System.out.println("El vehiculo tiene los siguientes datos:");
-        System.out.println("La patente: "+v.getPatente());
-        System.out.println("La marca: "+v.getMarca());
-        System.out.println("El año: "+v.getAnio());
-        System.out.println("La capacitad de carga en kg: "+v.getCapacidadCargaKg());
         System.out.println(v.toString());
     }
 

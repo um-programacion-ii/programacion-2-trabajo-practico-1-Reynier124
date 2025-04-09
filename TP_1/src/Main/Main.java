6bd409f8f7c746a9e5730f286c20a2f94bd9e284
Reynier124 <reynier1306@gmail.com>
2025-04-08 21:02:18 -0300

    Main sincronizado

--- TP_1/src/Main/Main.java
+++ TP_1/src/Main/Main.java
@@ -1,8 +1,8 @@
 package Main;
 import Class.Vehiculo;
-//import  Class.VehiculoPrinter;
-//import Class.Camion;
-//import Class.Auto;
+import  Class.VehiculoPrinter;
+import Class.Camion;
+import Class.Auto;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -10,34 +10,27 @@ import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
-        //VehiculoPrinter vp = new VehiculoPrinter();
+        VehiculoPrinter vp = new VehiculoPrinter();
 
         List<Vehiculo> vehiculos = new ArrayList<>();
 
-        //Camion c1 = new Camion("123RTYMA", "Renault", 2003, 903.23, true);
-        //Auto a1 = new Auto("093MAD123", "Ferrari", 2018, 83, 4);
-        Vehiculo v1 = new Vehiculo("123RTYMA", "Renault", 2003, 903.23);
-        Vehiculo v2 = new Vehiculo("093MAD123", "Ferrari", 2018, 83);
+        Camion c1 = new Camion("123RTYMA", "Renault", 2003, 903.23, true);
+        Auto a1 = new Auto("093MAD123", "Ferrari", 2018, 83, 4);
 
-        //vehiculos.add(c1);
-        //vehiculos.add(a1);
+        vehiculos.add(c1);
+        vehiculos.add(a1);
 
-        vehiculos.add(v1);
-        vehiculos.add(v2);
 
-        //vp.mostrarDatos(vehiculos.get(0));
-        vehiculos.get(0).mostrarInformacion();
+        vp.mostrarDatos(vehiculos.get(0));
         System.out.println("---------");
-        //vp.mostrarDatos(vehiculos.get(1));
-        vehiculos.get(1).mostrarInformacion();
+        vp.mostrarDatos(vehiculos.get(1));
 
         vehiculos = vehiculos.stream()
                 .filter(v -> v.getPatente().equalsIgnoreCase("123RTYMA"))
                 .collect(Collectors.toList());
 
         System.out.println("************");
-        //vp.mostrarVehiculos(vehiculos);
-        vehiculos.get(0).mostrarInformacion();
+        vp.mostrarVehiculos(vehiculos);
     }
 
 }
